667577ec07883295722bfc6f51ab6ffa646b776c
Sangyong-Jeon <80039556+Sangyong-Jeon@users.noreply.github.com>
2022-08-12 23:32:05 +0900

    [강의] 자바 8 API의 기본 메소드와 스태틱 메소드

--- project/java8to11/src/main/java/me/whiteship/java8to11/App.java
+++ project/java8to11/src/main/java/me/whiteship/java8to11/App.java
@@ -1,7 +1,22 @@
 package me.whiteship.java8to11;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+
 public class App {
     public static void main(String[] args) {
-        Foo.printAnything(); // Foo
+        List<String> name = new ArrayList<>();
+        name.add("myomyo");
+        name.add("eee");
+        name.add("aaa");
+        name.add("ddd");
+
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        name.sort(compareToIgnoreCase.reversed()); // 추가적으로 또 조건을 걸어 정렬할때
+
+        name.forEach(System.out::println);
+
     }
 }
