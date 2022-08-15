b5a301897cb8f85235ec1f246096c70d7d9ef2de
Sangyong-Jeon <80039556+Sangyong-Jeon@users.noreply.github.com>
2022-08-15 16:30:56 +0900

    [강의] Stream 소개

--- project/java8to11/src/main/java/me/whiteship/java8to11/App.java
+++ project/java8to11/src/main/java/me/whiteship/java8to11/App.java
@@ -1,22 +1,31 @@
 package me.whiteship.java8to11;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Spliterator;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        List<String> name = new ArrayList<>();
-        name.add("myomyo");
-        name.add("eee");
-        name.add("aaa");
-        name.add("ddd");
+        List<String> names = new ArrayList<>();
+        names.add("myomyo");
+        names.add("eee");
+        names.add("aaa");
+        names.add("ddd");
 
-        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
-        name.sort(compareToIgnoreCase.reversed()); // 추가적으로 또 조건을 걸어 정렬할때
+        // 일반 for문은 병렬로 처리하기 힘듬
+        for (String name : names) {
+            if (name.startsWith("m")) {
+                System.out.println(name.toUpperCase());
+            }
+        }
 
-        name.forEach(System.out::println);
+        // 스트림은 JVM이 알아서 병렬적으로 처리해줌
+        // 데이터가 정말 방대하게 큰 경우에는 병렬처리가 좋지만 일반적인 경우 그냥 스트림 쓰면 된다.
+        names.parallelStream()
+                .filter(s -> s.startsWith("m"))
+                .forEach(s -> System.out.println(s.toUpperCase()));
+
 
     }
 }
