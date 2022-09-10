3993abfd4dc0fee8ad3224396aa76895c3c2e123
JeonSangYong <tkddyd420@naver.com>
2022-09-10 22:25:44 +0900

    [강의] Executors

--- project/java8to11/src/main/java/me/whiteship/java8to11/App.java
+++ project/java8to11/src/main/java/me/whiteship/java8to11/App.java
@@ -1,22 +1,17 @@
 package me.whiteship.java8to11;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class App {
     public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            System.out.println("Thread: " + Thread.currentThread().getName());
-            try {
-                Thread.sleep(3000L);
-            } catch (InterruptedException e) {
-                // 현재 thread를 깨워서 예외처리
-            }
-        });
-        thread.start();
-        System.out.println("Hello: " + Thread.currentThread().getName());
-        try {
-            thread.join(); // 이 쓰레드 끝날때까지 main쓰레드는 기다림
-        } catch (InterruptedException e) { // 멀티 쓰레드 처리방법으로 쓰레드 한두개만해도 예외처리가 복잡해짐.
-            // 위 thread가 끝나는걸 대기중인데 main 쓰레드를 깨워서 예외처리
-        }
-        System.out.println(thread + " is finished");
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(getRunnable("Hello"), 1, 2, TimeUnit.SECONDS);
+    }
+
+    private static Runnable getRunnable(String message) {
+        return () -> System.out.println(message + Thread.currentThread().getName());
     }
 }
