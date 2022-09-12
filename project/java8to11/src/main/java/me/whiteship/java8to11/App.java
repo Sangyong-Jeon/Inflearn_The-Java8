34604a30c71a293d36359d99119af8e44b68e952
JeonSangYong <tkddyd420@naver.com>
2022-09-13 00:06:43 +0900

    [강의] Callable과 Future

--- project/java8to11/src/main/java/me/whiteship/java8to11/App.java
+++ project/java8to11/src/main/java/me/whiteship/java8to11/App.java
@@ -1,17 +1,39 @@
 package me.whiteship.java8to11;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class App {
-    public static void main(String[] args) {
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        executorService.scheduleAtFixedRate(getRunnable("Hello"), 1, 2, TimeUnit.SECONDS);
-    }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-    private static Runnable getRunnable(String message) {
-        return () -> System.out.println(message + Thread.currentThread().getName());
+        Callable<String> hello = () -> {
+            Thread.sleep(2000L);
+            return "Hello";
+        };
+
+        Callable<String> java = () -> {
+            Thread.sleep(3000L);
+            return "Java";
+        };
+
+        Callable<String> myomyo = () -> {
+            Thread.sleep(1000L);
+            return "Myomyo";
+        };
+
+        long l = System.currentTimeMillis();
+        String s = executorService.invokeAny(Arrays.asList(hello, java, myomyo));
+        System.out.println(s);
+        long l2 = System.currentTimeMillis();
+        System.out.println(l2 - l);
+        executorService.shutdown();
     }
 }
