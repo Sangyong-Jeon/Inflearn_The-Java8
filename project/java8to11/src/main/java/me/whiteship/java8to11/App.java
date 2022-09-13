4de15b6efe8d673ffeca41c520eed9449286fe22
JeonSangYong <tkddyd420@naver.com>
2022-09-13 23:58:47 +0900

    [강의]CompletableFuture 1

--- project/java8to11/src/main/java/me/whiteship/java8to11/App.java
+++ project/java8to11/src/main/java/me/whiteship/java8to11/App.java
@@ -3,6 +3,7 @@ package me.whiteship.java8to11;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,28 +13,14 @@ import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-
-        Callable<String> hello = () -> {
-            Thread.sleep(2000L);
-            return "Hello";
-        };
-
-        Callable<String> java = () -> {
-            Thread.sleep(3000L);
-            return "Java";
-        };
-
-        Callable<String> myomyo = () -> {
-            Thread.sleep(1000L);
-            return "Myomyo";
-        };
-
-        long l = System.currentTimeMillis();
-        String s = executorService.invokeAny(Arrays.asList(hello, java, myomyo));
-        System.out.println(s);
-        long l2 = System.currentTimeMillis();
-        System.out.println(l2 - l);
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("myomyo " + Thread.currentThread().getName());
+            return "myomyo";
+        }, executorService).thenRunAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+        }, executorService);
+        future.get();
         executorService.shutdown();
     }
 }
