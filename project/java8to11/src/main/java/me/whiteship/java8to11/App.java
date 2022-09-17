f465e8542f2873af2507d5df334497b4f9902bd5
JeonSangYong <tkddyd420@naver.com>
2022-09-17 21:36:15 +0900

    [강의] 배열 병렬 정렬

--- project/java8to11/src/main/java/me/whiteship/java8to11/App.java
+++ project/java8to11/src/main/java/me/whiteship/java8to11/App.java
@@ -1,21 +1,25 @@
 package me.whiteship.java8to11;
 
-import java.util.Arrays;
 
-@Chicken("양념")
-@Chicken("마늘간장")
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) {
-        // 클래스에서 getAnnotationsByType()에 Chicken 타입을 주면, 그 타입에 해당하는 애노테이션을 배열로 다 가져옴
-        Chicken[] chickens = App.class.getAnnotationsByType(Chicken.class);
-        Arrays.stream(chickens).forEach(c -> {
-            System.out.println(c.value());
-        });
+        int size = 1500;
+        int[] numbers = new int[size];
+        Random random = new Random();
 
-        // getAnnotation()은 애노테이션 1개만 가져오므로, 여기서는 ChickenContainer을 가져와서 감싸고 있는 값들을 전부 출력
-        ChickenContainer chickenContainer = App.class.getAnnotation(ChickenContainer.class);
-        Arrays.stream(chickenContainer.value()).forEach(c -> {
-            System.out.println(c.value());
-        });
+        // 1500개를 랜덤한 값으로 채움
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+        long start = System.nanoTime();
+        Arrays.sort(numbers); // 항상 싱글쓰레드 사용하는 퀵소트 사용함.
+        System.out.println("serial sorting took " + (System.nanoTime() - start));
+
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+        start = System.nanoTime();
+        Arrays.parallelSort(numbers); // 필요에 따라 여러 개의 쓰레드로 작업
+        System.out.println("parallel sorting took " + (System.nanoTime() - start));
     }
 }
